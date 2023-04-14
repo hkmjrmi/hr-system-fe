4dd25df774775e0c273acb0ac3ebdded43be8423
Hakim Jurimi <m.hakimjurimi@gmail.com>
2023-04-14 10:16:18 +0800

    create Public Holiday pages

--- src/Components/AppRoutes/AppRoutes.js
+++ src/Components/AppRoutes/AppRoutes.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Leave from "../../Pages/Leave";
 import Dashboard from "../../Pages/Dashboard";
 import Profile from "../../Pages/Profile";
-import Login from "../../Pages/Login";
+import ViewCalendar from "../ViewCalendar";
 
 function AppRoutes() {
     return ( 
@@ -11,6 +11,7 @@ function AppRoutes() {
         <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/leave" element={<Leave />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/calendar" element={<ViewCalendar />}></Route>
     </Routes>
 
     );
--- src/Components/SideMenu/SideMenu.js
+++ src/Components/SideMenu/SideMenu.js
@@ -1,3 +1,4 @@
+import { FieldTimeOutlined } from "@ant-design/icons";
 import { CalendarOutlined, DashboardOutlined, PoweroffOutlined, UserOutlined } from "@ant-design/icons";
 import { Menu } from "antd";
 import { useNavigate } from "react-router-dom";
@@ -16,10 +17,12 @@ function SideMenu({ logout }) {
           }
         }}
       >
-        <Menu.Item key="/dashboard" icon={<DashboardOutlined />} label="Dashboard" />
-        <Menu.Item key="/leave" icon={<CalendarOutlined />} label="Leave" />
-        <Menu.Item key="/profile" icon={<UserOutlined />} label="Profile" />
-        <Menu.Item key="/logout" icon={<PoweroffOutlined />} danger label="Logout" />
+        <Menu.Item key="/dashboard" icon={<DashboardOutlined />} label="Dashboard">Dashboard</Menu.Item>
+        <Menu.Item key="/leave" icon={<FieldTimeOutlined />} label="Leave">Leave</Menu.Item>
+        <Menu.Item key="/profile" icon={<UserOutlined />} label="Profile">Profile</Menu.Item>
+        <Menu.Item key="/calendar" icon={<CalendarOutlined />} label="Calendar">Public Holidays</Menu.Item>
+        <Menu.Item key="/logout" icon={<PoweroffOutlined />} danger label="Logout">Logout</Menu.Item>
+
       </Menu>
     </div>
   );
